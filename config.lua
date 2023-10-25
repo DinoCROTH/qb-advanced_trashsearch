9887d40210798d38f398a623d6b1a234799902f9
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2023-10-25 18:54:06 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,8 +4,31 @@ Config = {}
 Config.General = {
     -- Model of the trash bins -> can be entered as a string or as a number
     TrashBinModels = {
-        -1096777189, 666561306, 1437508529, -1426008804, -228596739, 161465839,
-        651101403,-58485588,218085040
+        -1096777189,
+        666561306,
+        1437508529,
+        -1426008804,
+        -228596739,
+        161465839,
+        651101403,
+        -58485588,
+        218085040,
+        1143474856,
+        1614656839,
+        1948359883,
+        -1187286639,
+        -230045366,
+        682791951,
+        1511880420,
+        1605769687,
+        388197031,
+        -1340926540,
+        -515278816,
+        1388308576,
+        600967813,
+        -206690185,
+        1329570871,
+        897494494,
     },
     -- Search distance for qb-target
     SearchDistance = 1.5,
@@ -28,22 +51,29 @@ Config.Stress = {
 
 Config.Reward = {
     -- Chance of getting a reward in % (0-100)
-    Chance = 75,
-    -- Money
-    Money = {
-        Chance = 25, -- Chance to get money instead of an item in % (0-100)
-        Min = 1,
-        Max = 100
-    },
+    Chance = 100,
+
     -- Minimal number of items that can be found
     MinNumberOfItems = 1,
     -- Maximal number of items that can be found
     MaxNumberOfItems = 5,
 
-    -- Items you can get
-    ItemList = {
-        "water_bottle", "metalscrap", "plastic", "copper", "glass", "lockpick",
-        "tunerlaptop","cryptostick","binoculars","lighter","beer","tosti"
+    -- Normal items you can get
+    NormalItems = {
+        Chance = 60, -- Chance to get a normal item in % (0-100)
+        ItemList = {"water_bottle", "metalscrap", "plastic", "copper", "glass", "lockpick"}
+    },
+
+    -- Rare items you can get
+    RareItems = {
+        Chance = 30, -- Chance to get a rare item in % (0-100)
+        ItemList = {"tunerlaptop","cryptostick","binoculars","lighter","beer","tosti"}
+    },
+
+    -- Very rare items you can get
+    VeryRareItems = {
+        Chance = 10, -- Chance to get a very rare item in % (0-100)
+        ItemList = {"diamond", "goldbar", "emerald"} -- Add your very rare items here
     }
 }
 
